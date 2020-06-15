9889dc83c040e9f23024b632dc3b07bf0e0f4004
martinezmatiasat <martinezmatiasat@gmail.com>
2020-06-15 16:37:48 -0300

    Se integra parte del front al back

---
+++ admin/adminLogin.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+
+require_once 'controllers/Alert.php';
+require_once 'controllers/user.php';
+
+if ($_POST) {
+    user::Loger($_POST["email"], $_POST["pass"]);
+}
+include_once 'partials/head.php';
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-6 text-center offset-xl-3">
+            <img src="" alt="Logo" class="img-fluid" style="width: 200px;">
+            <h2 class="mt-5 mb-3">ENLACE PROFESIONAL</h2>
+            <div>
+                <h3>Inicio de Sesión</h3>
+                <form class="form-inicio" action="adminLogin.php" method="post">
+                    <input class="icon-mail" name="email" type="email" placeholder="Email" required>
+                    <input class="icon-pass" name="pass" type="password" placeholder="Contraseña" required>
+                    <input type="hidden" name="csrf_token" value=<?php echo $_SESSION['csrf_token']['token']; ?>>
+                    <br>
+                    <a href="recuperar">No recuerdo mi clave</a>
+                    <br>
+                    <button class="form-boton" type="submit">INCIAR SESIÓN</button>
+                </form>
+            </div>
+            <!-- contenido -->
+            <?php echo Alert::show_msg(); ?>
+            <!-- ends -->
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php
-session_start();
-
-require_once 'controllers/Csrf.php';
-$csrf = new Csrf();
-
-header('Location: adminLogin.php');
+header('Location: home_web.php');
 exit();
